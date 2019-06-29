d8841b189935682070df9fc70f0bc62a48afdadc
Brett Jurman <i.be.brett@gmail.com>
2019-06-28 23:25:28 -0400

    adding prettier

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { RTMClient } = require('@slack/rtm-api');
+const { RTMClient } = require("@slack/rtm-api");
 
 const token = process.env.SLACK_BOT_TOKEN;
 
 const rtm = new RTMClient(token);
 
-rtm.on('message', (event) => {
-  if (event.text === 'd20') {
+rtm.on("message", event => {
+  if (event.text === "d20") {
     const roll = Math.floor(Math.random() * 20 + 1);
     rtm.sendMessage(`${roll}`, event.channel);
   }
