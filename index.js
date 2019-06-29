e10e9de239e3eb071c74a0ac812d845826d999ec
Brett Jurman <i.be.brett@gmail.com>
2019-06-29 19:04:15 -0400

    adding dotenv script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const token = process.env.SLACK_BOT_TOKEN;
 const rtm = new RTMClient(token);
 
 rtm.on("message", event => {
+  console.log('got message', event);
   if (event.text === "d20") {
     const roll = Math.floor(Math.random() * 20 + 1);
     rtm.sendMessage(`${roll}`, event.channel);
